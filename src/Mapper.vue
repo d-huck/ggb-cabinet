8643cce3402dfb18397aaeacf2d63024835b5bd4
dhuck <dhuck@tinycact.us>
2024-10-04 15:15:47 -0500

    move text to json public text for 10 in case of organic search refactor

--- src/Mapper.vue
+++ src/Mapper.vue
@@ -247,6 +247,7 @@ import phoneVideo from './assets/video/first_date.mp4';
 
 // Areas
 import areas from './assets/areas.json';
+import text from './assets/text.json';
 
 export default defineComponent({
   components: {
@@ -372,11 +373,6 @@ export default defineComponent({
         sounds.backgroundMusic.sound.value.level = MAX_LEVEL;
         sounds.backgroundMusic.play();
       }, 50);
-      // setTimeout(() => {
-      //   // console.log("Setting image map areas");
-      //   areasData.value = areas;
-      //   handleResize();
-      // }, 100);
     };
 
     const closeTextDialog = () => {
@@ -455,30 +451,23 @@ export default defineComponent({
 
     const submitCode = () => {
       //let lockedCode = lockedCode1.value + "/" + lockedCode2.value + "/" + lockedCode3.value;
-      if (date.value &&
-        (lockedCode.value === "04/15/2015") ||
-        (lockedCode.value === "4/15/2015") ||
-        (lockedCode.value === "04/15/15") ||
-        (lockedCode.value === "4/15/15")
-      ) {
-        let phone = "270-316-8424";
-        let address = "Lost Horizon Cidery Mill, 3770 FM1854, Dale, TX 78616";
-        
-        showLockedDrawerDialog.value = false;
-        textDialog.value = `Emily,<br>
-        <br>
-        If you&#8217;re reading this, then it&#8217;s likely you haven&#8217;t heard from me in a little while. I left something for you in my camper that I hope will explain things. It's parked at ${address} and I've arranged someone to meet you there on ${date.value}. 
-        <br />
-        <br>Call ${phone} when you arrive and they'll let you in. Don&#8217;t be late.<br>
-        <br>
-        -Henry;`;
+      if (lockedCode.value === text["date"]) {
+        console.log("Locked code:", lockedCode.value);
         let choice = Math.floor(Math.random()*sounds.drawer.open.length);
         sounds.drawer.open[choice]();
-      } else {
         showLockedDrawerDialog.value = false;
-        let choice = Math.floor(Math.random()*sounds.drawer.stuck.length);
-        sounds.drawer.stuck[choice]();
+        if (date.value) {
+          textDialog.value = text["10-protected"];
+        } else {
+          textDialog.value = text["10-public"];
+        }
       }
+    };
+
+    const getText = (key) => {
+      console.log("Getting text for key:", key);
+      console.log("Text:", text[key]);
+      return text[key];
     };
 
     const handleImageMapClick = (area, index, event) => {
@@ -490,15 +479,9 @@ export default defineComponent({
         sounds.drawer.open[choice]();
       } 
 
+
+      
       switch (area.name) {
-        case "1":
-          textDialog.value = `Jen was naturally gifted at virtually every art form she ever touched, but she refused to call herself an artist because &#8220;artists actually finish projects.&#8221; She would tell people she was a dilettante at best and laugh, but I knew it was a sore spot for her. I asked her once why she didn&#8217;t finish her projects, and she said it was because the thing she made was never as pure as the idea of it, so she would chase the idea until it stopped being real. At some point, she would inevitably feel frustrated and abandon her work.<br>
-          <br>
-          The one exception to this was the graphic novel she had been slowly, secretly working on for five years. It was easily the longest she had ever stuck with a project, but progress was slow because &#8220;the true form kept changing.&#8221; So she would begin again and again. Instead of feeling frustrated, it was a self-renewing source of discovery. The point was not to finish, but to capture the truer, deeper cut as it emerged in the moment. It was only ever meant for her. I wish I knew what it was even about, but it&#8217;s all lost to the flood.`;
-          break;
-        case "2":
-          textDialog.value = `On days when Jen was too depressed to get out of bed, I would stay in bed with her and make up these dumb little stories that she called &#8220;can&#8217;t-get-out-of-bed-time stories.&#8221; Honestly, I would just say whatever nonsense popped into my head to try to make her laugh. We would do that for hours sometimes, me just holding her or playing with her hair as she drifted in and out of consciousness. I would give anything for even just one of our worst days together again.`;
-          break;
         case "40":
           soundPlaying.value = sounds.pianoAudio;
           sounds.backgroundMusic.sound.value.fade(MAX_LEVEL, 0, FADE_OUT);
@@ -506,18 +489,6 @@ export default defineComponent({
             sounds.backgroundMusic.pause();
           }, FADE_OUT);
           bgm_pos = sounds.backgroundMusic.sound.value.seek();
-          break;
-        case "4":
-          textDialog.value = `I remember Jen telling me about a strange experience she had as a girl. She remembers playing in her room&mdash;in this 90-year-old farmhouse and seeing a teenage girl in strange old clothing watching her with curiosity. She felt excited to have the undivided attention of someone older than her. 
-          <br />
-          <br />She ran to show her mom the girl, but when they returned, she was nowhere. Jen told me that she was so desperate for attention, she began to imagine that the girl was watching her with great interest all of the time, even though she only occasionally saw her.  Making her bed or brushing her teeth was now an event worthy of an audience. Jen named the girl &#8220;Chrissy&#8221; and talked to her constantly. Her parents and teachers just assumed this was an imaginary friend of hers and that she would eventually outgrow it. 
-          <br />
-          <br/>Jen was 11 or 12 when it first occurred to her that Chrissy was a dead person. The feeling of being watched changed after that. She stopped talking to or acknowledging Chrissy altogether. She said she&#8217;s never told anyone about that before, but it&#8217;s part of the reason she doesn&#8217;t like going back home. The other part was her parents. Maybe it was the power of suggestion, but sleeping in her childhood bedroom, I felt it too. I never told her that. I don&#8217;t know why.`;
-          break;
-        case "5":
-          textDialog.value = `I remember waiting for the moment I would do or say something too weird or vulnerable and she would recoil and slink away. Everyone who had ever tried to love me eventually realized that they couldn&#8217;t for reasons I and possibly they will never fully understand.<br>
-          <br>
-          When she saw the MRE stockpile and go bag in the camper, she asked me if I was a prepper. I explained to her that I was prepping for climate change and the societal collapse that follows. I kept thinking to myself: this is it. This is how I lose her. But she just listened. She never ridiculed me or told me I was overreacting.`;
           break;
         case "6":
           sounds.backgroundMusic.sound.value.fade(MAX_LEVEL, 0, FADE_OUT);
@@ -535,37 +506,12 @@ export default defineComponent({
             }]
           };
           break;
-        case "7":
-          textDialog.value = `I came home one day to find Jen staring at a wall and unresponsive. When I touched her arm, she flinched. I thought something terrible had happened, and in a way it had, but it was the slow, wearing down of a person, rather than any single thing. She worked for a company that exclusively catered events for the super wealthy. The tips were great and for a little while, Jen thought the stories of people placing chewed up gum in her hand or offering her $10k to sleep with them were almost funny.<br>
-          <br>
-          But once the novelty wore off, it was completely demoralizing. She stopped creating and she had frequent night terrors. I told her to just quit and that I could float us both for a little while, but she didn&#8217;t think she would be able to find anything that paid enough to keep up with our rent hike. I told her, &#8220;Let&#8217;s just move then. Nothing is worth you feeling like this.&#8221;  She thought I meant to another apartment in L.A., but when I suggested we leave California and find somewhere out in the country to homestead, it was the first time she looked truly relieved in months.`;
-          break;
-        case "8":
-          textDialog.value = `Our first year homesteading in West Virginia was beautiful and unforgiving. There was joy and heartache and the luxury of boredom. There was powdery mildew and blackberries and toil and chickens. We lived in a doublewide trailer on 10 acres of land that no one wanted. We collected rain water and grew and canned enough vegetables to feed ourselves that winter. 
-          <br />
-          <br />It was Jen&#8217;s idea to plant a half acre of sunflowers and zinnias, which not only looked beautiful but was also a decent source of income. People would come and pick their own bouquets and buy some veggies while they were already there. I watched my wife wake back up to herself again. She was tired in a way that was satisfying. She began creating again.`;
-          break;
         case "9":
           flipbookDisplay.value = true;
           break;
         case "10":
-          if (date.value) {
-            showLockedDrawerDialog.value = true;
             lockedCode.value = "";
-          }
-          break;
-        case "11":
-          textDialog.value = `Jen found a nest of baby rabbits. She knew to leave them alone under normal circumstances, but their mother never returned. The nearest wild rehabilitation was already at capacity, so she bottle fed them every two hours for eight days, only taking breaks when I could relieve her for a nap. We lost one of them, but the others managed to survive long enough to feed themselves. She built them a rabbit hutch in the backyard and frequently brought them inside to play. She cared for them for over a year.<br>
-          <br>
-          All it took was one time for me to forget to latch the hutch for a fox to get inside. It was such a small and simple thing. I don&#8217;t even remember what distracted me. We woke up to the sound of their screams. I immediately knew what was happening and told her &#8220;don&#8217;t look&#8221; and that I would take care of it, but she very tenderly gathered them up in an old dress and buried them herself. Then she didn&#8217;t speak to me or leave the bed for three days. `;
-          break;
-        case "12":
-          textDialog.value = `The only time I&#8217;ve ever heard Jen sing in nearly ten years of knowing her was secretly. And to the tomatoes. She would make up these silly little songs to encourage them to grow and I hid behind the screen door, straining to hear all the words. Her singing wasn&#8217;t great, but it was just so sweet. I loved it. I wish I had told her that, but I think she probably would have stopped if I had.`;
-          break;
-        case "13":
-          textDialog.value = `Our second year in West Virginia my older sister, Patty, was diagnosed with breast cancer. Emily&#8217;s dad wasn&#8217;t in the picture and Patty didn&#8217;t have the energy to take care of Emily alone while she underwent chemo, so they came to live with us for the summer. I remember how natural Jen was with Emily. She knew how to talk to her without talking down to or over her. There was an ease between them that I couldn&#8217;t fake even though I tried. I felt embarrassed because she was my niece, but in hindsight I think Emily was too scared that her mom might die to notice my awkwardness. 
-          <br />
-          <br />I did what I could to distract her with something fun by creating increasingly elaborate scavenger hunts and by helping her direct her first movie on an old camcorder I got at an estate sale. We gave them our bedroom and slept on an air mattress in the living room. I remember Jen whispering while they were sleeping that we should be prepared to adopt Emily if Patty dies. I agreed, but I was terrified.`;
+            showLockedDrawerDialog.value = true;
           break;
         case "14":
           soundPlaying.value = sounds.proposalAudio;
@@ -575,14 +521,8 @@ export default defineComponent({
           }, FADE_OUT);
           bgm_pos = sounds.backgroundMusic.sound.value.seek();
           break;
-        case "15":
-          textDialog.value = `She could be secretive. She never copped to smoking the whole time I knew her, but I would occasionally find a pack hidden in an odd place or a butt snuffed out in a flower pot. I was concerned for her health, but I was fine with it. It seemed infrequent enough, but I don&#8217;t know why she felt the need to hide it from me.`;
-          break;
         case "16":
           imageDisplay.value = letterImage;
-          break;
-        case "19":
-          textDialog.value = `After Emily stayed with us the summer Patty was sick, Jen told me she realized she wanted to have a child. I felt betrayed. I asked her what was so wrong with our life that she felt she needed a kid to fix it. I didn&#8217;t want to get left behind, or worse, for her to realize I wouldn&#8217;t be a good dad. I was so afraid of losing her that I almost did. Sometimes I wonder if she had left, would she have been happier? At least she'd still be alive.`;
           break;
         case "20":
           soundPlaying.value = sounds.voicemailAudio;
@@ -592,13 +532,8 @@ export default defineComponent({
           }, FADE_OUT);
           bgm_pos = sounds.backgroundMusic.sound.value.seek();
           break;
-        case "22":
-          textDialog.value = `I found out she was pregnant from the coroner. When was she planning to tell me? Was she going to go through with it? Did she even know?`;
-          break;
-        case "23":
-          textDialog.value = `I keep growing older but you are always thirty-two. I didn&#8217;t think about it at first, but the gap between us just keeps widening. Sometimes I think about a future memory that doesn&#8217;t exist. It&#8217;s very simple. Just a  5-second film reel of lines stretching across your face as you smile at me, silver threading through your hair. You&#8217;re gorgeous.`;
-          break;
         default:
+          textDialog.value = getText(area.name);
           break;
       }
     };
